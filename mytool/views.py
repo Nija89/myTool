a69b2fb7560c10480267a767c024b9258726b4a3
Nija Tandukar <nijatandukar18@gmail.com>
2024-12-18 19:47:59 +0545

    Another Commit

--- mytool/views.py
+++ mytool/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse,redirect
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import addTask
 from .models import firstDB
 
@@ -8,14 +8,14 @@ def firsFunction(request):
     context = {
         'apple' : myList
     }
-    return render(request, "index.html", context)
+    return render(request, 'mytool/index.html', context)
     # return HttpResponse("Hello World")
 
 def success(request):
-    return render(request, "success.html")
+    return render(request, 'mytool/success.html')
 
 def fail(request):
-    return render(request, "fail.html")
+    return render(request, 'mytool/fail.html')
 
 def add_task(request):
     if request.method == "POST":
@@ -30,13 +30,13 @@ def add_task(request):
 
             # Create a context with name and description for the success page
             context = {'name': name, 'description': description}
-            return render(request, 'success.html', context)  # Render the success page
+            return render(request, 'mytool/success.html', context)  # Render the success page
         else:
             # If the form is not valid, show the form again with errors
             print(form.errors)
-            return render(request, 'add_task.html', {'form': form})  # Show the form again for corrections
+            return render(request, 'mytool/add_task.html', {'form': form})  # Show the form again for corrections
 
     else:
         form = addTask()  # Create a new empty form
 
-    return render(request, 'add_task.html', {'form': form})  # Render the form page initially
+    return render(request, 'mytool/add_task.html', {'form': form})  # Render the form page initially
