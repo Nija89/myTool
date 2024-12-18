7cf3a76af0b824cdda368bd10637306c3efcaa3c
Nija Tandukar <nijatandukar18@gmail.com>
2024-12-18 20:52:17 +0545

    another changes

--- mytool/models.py
+++ mytool/models.py
@@ -6,3 +6,5 @@ class firstDB(models.Model):
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
     
+    class Meta:
+        db_table = 'mytool_firstdb'
